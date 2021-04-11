781c1bcce7b5aa47c67d0aef9d3818a3958f8c6d
ayoung0073 <ayong0310@naver.com>
2021-04-12 01:12:13 +0900

    Add : AOP 값 넘겨주기 테스트

--- src/main/java/com/may/aop/LoginCheckAspect.java
+++ src/main/java/com/may/aop/LoginCheckAspect.java
@@ -1,7 +1,10 @@
 package com.may.aop;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,23 +16,27 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Aspect
+@RequiredArgsConstructor
 @Component
 public class LoginCheckAspect {
+
+    private final MemberRepository memberRepository;
 
     @Pointcut("execution(* com.may..*Controller.check*(..))")
     public void loginCheckPointcut() {}
 
 //    @Before("loginCheckPointcut()")
-    @Before("@annotation(LoginCheck)") // 다른 패키지에 LoginCheck 인터페이스 정의되어 있으면, 패키지까지 작성
-    public void loginCheck(JoinPoint jp) {
+    @Around("@annotation(LoginCheck)") // 다른 패키지에 LoginCheck 인터페이스 정의되어 있으면, 패키지까지 작성
+    public void loginCheck(ProceedingJoinPoint pjp) throws Throwable {
 
         log.info("Login Check");
-        HttpSession session = (HttpSession)jp.getArgs()[0]; // 인자 Object 배열의 첫번째 원소 : HttpSession session
+        HttpSession session = (HttpSession)pjp.getArgs()[0]; // 인자 Object 배열의 첫번째 원소 : HttpSession session
         String id = SessionUtil.getLoginMemberId(session);
 
         if(id == null) {
             throw new HttpStatusCodeException(HttpStatus.UNAUTHORIZED, "로그인 실패") {};
         }
 
+        pjp.proceed(new Object[]{session, memberRepository.findById(id).get()});
     }
 }
--- src/main/java/com/may/aop/MemberRepository.java
+++ src/main/java/com/may/aop/MemberRepository.java
@@ -2,5 +2,7 @@ package com.may.aop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberDto, String> {
 }
