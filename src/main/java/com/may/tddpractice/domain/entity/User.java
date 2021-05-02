554bf72e6e7b6a0a69cb1f8eb73b17c6018897fa
ayoung0073 <ayong0310@naver.com>
2021-05-02 13:29:14 +0900

    Add : UserService 통합 테스트 추가
    - 회원가입 테스트
    - 회원 중복 예외 테스트

--- src/main/java/com/may/tddpractice/domain/entity/User.java
+++ src/main/java/com/may/tddpractice/domain/entity/User.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +15,10 @@ import javax.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     private int age;
--- src/test/java/com/may/tddpractice/service/UserServiceTest.java
+++ src/test/java/com/may/tddpractice/service/UserServiceTest.java
@@ -5,10 +5,11 @@ import com.may.tddpractice.domain.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
+
+import static org.assertj.core.api.Assertions.*;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,7 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
-@ExtendWith(MockitoExtension.class) // 이 코드 없으면 NPE
+@ExtendWith(MockitoExtension.class)
+        // 이 코드 없으면 NPE
 class UserServiceTest {
 
     @Mock
@@ -48,7 +50,7 @@ class UserServiceTest {
         List<User> findUserList = userRepository.findAll();
 
         // then
-        assertThat(userList, is(findUserList));
+        assertThat(findUserList).isEqualTo(userList);
     }
 
 //    @Test
@@ -68,8 +70,9 @@ class UserServiceTest {
 //        // UserRepository save 메서드 호출했는지 확인
 //    }
 
-    @BeforeEach // BeforeAll : static 메서드로 선언해줘야 함
-    void setup(){
+    @BeforeEach
+        // BeforeAll : static 메서드로 선언해줘야 함
+    void setup() {
         User user1 = User.builder()
                 .age(7)
                 .name("문마음")
@@ -98,7 +101,7 @@ class UserServiceTest {
         List<User> userList = userRepository.findAll();
 
         // then
-        assertThat(2, is(userList.size()));
+        assertThat(userList.size()).isEqualTo(2);
     }
 
     @Test
@@ -110,7 +113,7 @@ class UserServiceTest {
         List<User> userList = userService.findAll();
 
         // then
-        assertThat(2, is(userList.size()));
+        assertThat(userList.size()).isEqualTo(2);
     }
 
 
