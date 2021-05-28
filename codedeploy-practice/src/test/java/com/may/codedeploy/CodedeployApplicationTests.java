bd39945ba708b1ab521678c8a80f2a932fb0c6ba
ayoung0073 <ayong0310@naver.com>
2021-05-29 00:51:12 +0900

    Chore : 테스트 프로젝트 생성

---
+++ codedeploy-practice/src/test/java/com/may/codedeploy/CodedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.codedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
