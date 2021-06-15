7120d73189e6fa7e0549bdd59149578a811ab00b
ayoung0073 <ayong0310@naver.com>
2021-06-15 13:45:50 +0900

    Fix : MimeMessageHelper 제거

--- sprintboot-mail/src/main/java/com/may/email/EmailService.java
+++ sprintboot-mail/src/main/java/com/may/email/EmailService.java
@@ -3,7 +3,6 @@ package com.may.email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -19,34 +18,34 @@ public class EmailService {
     private final JavaMailSender emailSender;
     private final SpringTemplateEngine templateEngine;
 
-    private String createKey() {
-        StringBuilder key = new StringBuilder();
+    private String createCode() {
+        StringBuilder code = new StringBuilder();
         Random rnd = new Random();
         for (int i = 0; i < 7; i++) {
             int rIndex = rnd.nextInt(3);
             switch (rIndex) {
                 case 0:
-                    key.append((char) (rnd.nextInt(26) + 97));
+                    code.append((char) (rnd.nextInt(26) + 97));
                     break;
                 case 1:
-                    key.append((char) (rnd.nextInt(26) + 65));
+                    code.append((char) (rnd.nextInt(26) + 65));
                     break;
                 case 2:
-                    key.append((rnd.nextInt(10)));
+                    code.append((rnd.nextInt(10)));
                     break;
             }
         }
-        return key.toString();
+        return code.toString();
     }
 
     public void sendEmailMessage(String email) throws Exception {
-        String code = createKey();
+        String code = createCode();
         MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setTo(email);
-        helper.setSubject("[인증 코드] " + code);
 
-        helper.setText(setContext(code), true); // 기본값 text 이므로 html: true 설정
+        message.addRecipients(MimeMessage.RecipientType.TO, email);
+        message.setSubject("[인증 코드] " + code);
+        message.setText(setContext(code), "utf-8", "html");
+
         emailSender.send(message);
     }
 
