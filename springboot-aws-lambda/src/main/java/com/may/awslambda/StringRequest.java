01bc7d17abe80d90a9eb986b3512d30751849d8a
ayoung0073 <ayong0310@naver.com>
2021-07-11 22:45:42 +0900

    Add : Function Local Test

---
+++ springboot-aws-lambda/src/main/java/com/may/awslambda/StringRequest.java
@@ -0,0 +1,9 @@
+package com.may.awslambda;
+
+import lombok.Getter;
+
+@Getter
+public class StringRequest {
+    private String input;
+    private int length;
+}
---
+++ springboot-aws-lambda/src/main/java/com/may/awslambda/StringValidator.java
@@ -0,0 +1,19 @@
+package com.may.awslambda;
+
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component // 하나의 함수
+public class StringValidator implements Function<StringRequest, StringResponse> {
+    @Override
+    public StringResponse apply(StringRequest stringRequest) {
+        String input = stringRequest.getInput();
+
+        if (input.length() != stringRequest.getLength()) {
+            return new StringResponse(false);
+        }
+
+        return new StringResponse(true);
+    }
+}
