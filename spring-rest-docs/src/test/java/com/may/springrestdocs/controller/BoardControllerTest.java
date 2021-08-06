213fcabeb22376531cba5a231e680b86d875b4a0
ayoung0073 <ayong0310@naver.com>
2021-08-06 22:43:57 +0900

    Fix : gradle version
    - 7.1.1 -> 6.9

--- spring-rest-docs/src/test/java/com/may/springrestdocs/controller/BoardControllerTest.java
+++ spring-rest-docs/src/test/java/com/may/springrestdocs/controller/BoardControllerTest.java
@@ -3,20 +3,15 @@ package com.may.springrestdocs.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.may.springrestdocs.dto.BoardResponse;
 import com.may.springrestdocs.service.BoardService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static com.may.springrestdocs.config.ApiDocumentUtils.getDocumentRequest;
 import static com.may.springrestdocs.config.ApiDocumentUtils.getDocumentResponse;
@@ -34,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
 public class BoardControllerTest {
